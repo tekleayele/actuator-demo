a1eef451c48f4162b52b159022da30c4824e031d
tekleayele@gmail.com <tekleayele@gmail.com>
2018-08-02 13:04:01 -0400

    Initial Commit.

---
+++ src/main/java/org/tekle/spring/boot/actuator/ActuatorsDemoApplication.java
@@ -0,0 +1,13 @@
+package org.tekle.spring.boot.actuator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActuatorsDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ActuatorsDemoApplication.class, args);
+    }
+
+} 
---
+++ src/main/java/org/tekle/spring/boot/actuator/CustomHealthCheck.java
@@ -0,0 +1,25 @@
+package org.tekle.spring.boot.actuator;
+
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CustomHealthCheck implements HealthIndicator{
+
+    @Override
+    public Health health() {
+        int errorCode = check(); // perform some specific health check
+        if (errorCode != 0) {
+            return Health.down()
+              .withDetail("Error Code", errorCode).build();
+        }
+        return Health.up().build();
+    }
+     
+    public int check() {
+        // Our logic to check health
+        return 0;
+    }
+ 
+}
